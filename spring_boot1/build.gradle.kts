233ecc4ac6e542df14c09bf679c7b54990c55e82
Snehil <snehilkumar94@gmail.com>
2026-01-17 03:16:25 +0530

    updated

--- spring_boot1/build.gradle.kts
+++ spring_boot1/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-	kotlin("jvm") version "2.2.21"
-	kotlin("plugin.spring") version "2.2.21"
-	id("org.springframework.boot") version "4.0.1"
+	kotlin("jvm") version "1.9.25"
+	kotlin("plugin.spring") version "1.9.25"
+	id("org.springframework.boot") version "3.4.3"
 	id("io.spring.dependency-management") version "1.1.7"
 }
 
